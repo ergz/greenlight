147bbb3e81db0815e196f253ef80afd7e0ca19ae
ergz <e.emanuel.rodriguez@gmail.com>
2023-06-19 02:24:23 -0700

    start using Marshal for creating json responses

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -1,13 +1,30 @@
 package main
 
 import (
-	"fmt"
+	"encoding/json"
 	"net/http"
 )
 
 func (app *application) healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "environment: %s\n", app.config.env)
-	fmt.Fprintf(w, "version: %s", version)
+
+	// create a mapping that will be converted to json
+	data := map[string]string{
+		"status":      "available",
+		"environment": app.config.env,
+		"version":     version,
+	}
+
+	js, err := json.Marshal(data)
+	if err != nil {
+		app.logger.Print(err)
+		http.Error(w, "the server encountered an error and could process your request", http.StatusInternalServerError)
+		return
+	}
+
+	js = append(js, '\n')
+
+	w.Header().Set("Content-Type", "application/json")
+
+	w.Write(js)
 
 }
